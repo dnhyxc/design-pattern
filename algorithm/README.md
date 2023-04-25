44071f87ef6e78effc2b42b75db8587897171b74
dengnh <dengninghai@shinemo.com>
2023-04-25 17:58:05 +0800

    feat：完善算法

---
+++ algorithm/README.md
@@ -0,0 +1,49 @@
+### 查找字符串中连续出现最多次数的字符及个数
+
+```js
+function findLetterRepeatCount(str) {
+  const context = {
+    startPos: 0, // 起始位置
+    cursor: 0, // 指针向右移动的位置
+    maxCount: 0, // 最大数量
+    letter: "", // 最长字符
+    str, // 源数据
+  };
+
+  if (context.cursor === context.str.length) {
+    return {
+      maxCount: 1,
+      letter: context.str[0] || "",
+    };
+  }
+
+  while (context.cursor < context.str.length) {
+    moveCursor(context);
+  }
+
+  return {
+    maxCount: context.maxCount,
+    letter: context.letter || "",
+  };
+}
+
+function moveCursor(context) {
+  while (
+    context.str[context.cursor] === context.str[context.startPos] &&
+    context.cursor < context.str.length
+  ) {
+    context.cursor++;
+  }
+
+  if (context.cursor - context.startPos > context.maxCount) {
+    context.maxCount = context.cursor - context.startPos;
+    context.letter = context.str[context.cursor - 1];
+  }
+
+  context.startPos = context.cursor;
+}
+
+const testStr = "";
+const result = findLetterRepeatCount(testStr);
+console.log(result, "result");
+```
--- algorithm/js/scan.js
+++ algorithm/js/scan.js
@@ -5,11 +5,11 @@ class Scaner {
 
   createContext(targetStr) {
     this.context = {
-      startPos: 0,
-      cursor: 0,
-      maxCount: 0,
-      letter: "",
-      str: targetStr,
+      startPos: 0, // 起始位置
+      cursor: 0, // 指针向右移动的位置
+      maxCount: 0, // 最大数量
+      letter: "", // 最长字符
+      str: targetStr, // 源数据
     };
   }
 
@@ -17,34 +17,50 @@ class Scaner {
     return this.context || {};
   }
 
-  moveCursor(context) {
-    while (context.str[context.startPos] === context.startPos) {
-      context.cursor++;
-    }
-    if (context.cursor - context.startPos > context.startPos) {
-      context.maxCount = context.cursor - context.startPos;
-      context.letter = context.str[context.startPos];
-    }
-    context.startPos = context.cursor;
-  }
-
   findLetterRepeatCount() {
     const context = this.getContext();
     context.startPos = 0;
     context.cursor = 1;
-    if (context.str.length === context.cursor) {
+    // 如果指针向右移动的位置等于字符长度，则直接输出结果
+    if (context.cursor === context.str.length) {
+      // 1 => 4
       return {
         maxCount: 1,
         letter: context.str[0],
       };
     }
+    // 如果指针向右移动的位置小于字符长度，则进入循环
     while (context.cursor < context.str.length) {
       this.moveCursor(context);
     }
+    // 如果指针向右移动的位置等于字符长度，则输出结果
     return {
       maxCount: context.maxCount,
       letter: context.letter,
     };
+  }
+
+  // 移动指针
+  moveCursor(context) {
+    // 如果起始位置的字符等于指针移动位置的字符，并且指针移动的位置小于字符的最大长度，则进入循环
+    // 如果起始位置的字符不等于指针移动位置的字符，则指针位置不移动
+    while (
+      context.str[context.startPos] === context.str[context.cursor] &&
+      context.cursor < context.str.length
+    ) {
+      // 指针持续向后移动
+      context.cursor++; // 4 =>
+    }
+
+    // 如果指针移动的位置减去起始位置的大小大于原先存入的最大数量，则存入的最大数量更新为：指针移动的位置减去起始位置的大小
+    if (context.cursor - context.startPos > context.maxCount) {
+      context.maxCount = context.cursor - context.startPos; // 4
+      // 最长字符标记为当前指针移动所在的这个位置
+      context.letter = context.str[context.cursor - 1]; // d
+    }
+
+    // 将起始位置变更为指针移动位置
+    context.startPos = context.cursor; // 此时的位置正好是下一个不一样的字符所在位置
   }
 }
 
