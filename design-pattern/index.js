ce0d164492fa73ff523e855087d7a9ff03b9e7b0
dnhyxc <2640559435@qq.com>
2023-05-05 23:34:52 +0800

    feat：组合模式

--- design-pattern/index.js
+++ design-pattern/index.js
@@ -1,1 +1,3 @@
 import "./src/combined";
+import "./src/subpub";
+import "./src/observer";
--- design-pattern/src/combined/index.js
+++ design-pattern/src/combined/index.js
@@ -1,6 +1,11 @@
 // 组合模式
+
+console.log("============组合模式begin=============");
+
 const combined = () => {
   console.log("组合模式");
 };
 
 combined();
+
+console.log("============组合模式end=============");
